8800fd8e9b73628216d0b89b43fb3eae11316b87
Egor Shakhmin <lapter57@gmail.com>
2020-09-23 04:49:59 +0300

    Added stress test for lab12 (mpi)

---
+++ openmp/lab12/stress_test.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+MPIRUN = ("mpirun -n {num_process} main -r={r} -x=2 --test")
+
+
+def getMeanTime():
+    time_arr = None
+    with open('time.txt') as f:
+        time_arr = np.array([float(x[:-1]) for x in f])
+    os.remove("time.txt")
+    return np.mean(time_arr)
+
+
+def test(num_process, r, iterations):
+    cmd = MPIRUN.format(num_process=num_process, r=r)
+    for i in range(iterations):
+        os.system(cmd)
+    return getMeanTime()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', type=int, default=1000,
+                        help="Number of iterations")
+
+    args = parser.parse_args()
+    x = np.array([i for i in range(2, 22, 2)])
+    for p in tqdm(range(4)):
+        y = np.array([])
+        for r in tqdm(range(2, 22, 2)):
+            y = np.append(y, test(p + 1, r, args.n))
+        plt.plot(x, y, label="num_process = {}".format(p + 1))
+
+    plt.xlabel('r')
+    plt.ylabel('time (ms)')
+    plt.legend()
+    plt.savefig('test.png')

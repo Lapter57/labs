4baf36f17629f28b97be1b77ac3f7070eba57059
Egor Shakhmin <lapter57@gmail.com>
2020-10-20 21:51:46 +0300

    Created common gradle config for bdas

--- bdas/lab1/build.gradle.kts
+++ bdas/lab1/build.gradle.kts
@@ -1,45 +1,9 @@
-plugins {
-    java
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-}
-
-group = "ru.spbstu.shakhmin"
 version = "1.0-SNAPSHOT"
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
-}
-
-repositories {
-    jcenter()
-}
-
-tasks {
-    test {
-        maxHeapSize = "256m"
-        useJUnitPlatform()
-    }
-}
-
 dependencies {
-    // Annotations for better code documentation
-    implementation("com.intellij:annotations:12.0")
-
     // Apache commons-cli
     implementation("commons-cli:commons-cli:1.4")
 
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter")
-
-    // JUnit Jupiter test framework
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-
-    val lombok = "org.projectlombok:lombok:1.18.12"
-    compileOnly(lombok)
-    annotationProcessor(lombok)
-    testCompileOnly(lombok)
-    testAnnotationProcessor(lombok)
 }
